c7a26ebcad22b921f335aeb7c607f541e30e5e6f
Wonjae Ban <wban@student.42seoul.kr>
2021-10-13 12:01:42 +0900

    ex05m

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -6,37 +6,38 @@
 /*   By: wban <wban@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 21:45:02 by wban              #+#    #+#             */
-/*   Updated: 2021/10/12 17:10:26 by wban             ###   ########.fr       */
+/*   Updated: 2021/10/13 12:00:00 by wban             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<unistd>
+#include<unistd.h>
 
 void	ft_print_comb(void)
-{
-	int	num[2];
+{	
+	char	num[3];
 
-	num[0] = 0;
-	num[1] = 1;
-	num[2] = 2;
-	while (num[0] <= 7)
+	num[0] = '0';
+	while (num[0] <= '7')
 	{
-		while (num[1] <= 8)
+		num[1] = num[0] + 1;
+		while (num[1] <= '8')
 		{
-			while (num[2] <= 9)
+			num[2] = num[1] + 1;
+			while (num[2] <= '9')
 			{	
 				if ((num[2] != num[0]) && (num[2] != num[1]))
 				{
 					write(1, num, 1);
 					write(1, num + 1, 1);
 					write(1, num + 2, 1);
+					if (num[0] == '7' && num[1] == '8' && num[2] == '9')
+						break ;
+					write(1, ", ", 2);
 				}
 				num[2]++;
 			}
 			num[1]++;
-			num[2] == 0;
 		}
 		num[0]++;
-		num[1] == 0;
 	}
 }
