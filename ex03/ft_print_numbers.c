b65aefcaf50b61dd96b4aa89a0964da2f876e147
Wonjae Ban <wban@student.42seoul.kr>
2021-10-13 09:48:49 +0900

    ex03m

--- ex03/ft_print_numbers.c
+++ ex03/ft_print_numbers.c
@@ -6,7 +6,7 @@
 /*   By: wban <wban@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 19:16:02 by wban              #+#    #+#             */
-/*   Updated: 2021/10/11 21:24:24 by wban             ###   ########.fr       */
+/*   Updated: 2021/10/13 09:38:01 by wban             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 void	ft_print_numbers(void)
 {
-	int	number;
-	int	result;
-	int	s;
+	long	number;
+	int		s;
+	char	result;
 
 	number = 9876543210;
 	s = number / 10;
-	result = number % 10;
+	result = (number % 10) + '0';
 	while (1)
 	{
 		write(1, &result, 1);
 		if (s == 0)
 			break ;
-		result = s % 10;
+		result = (s % 10) + '0';
 		s /= 10;
 	}
 }
